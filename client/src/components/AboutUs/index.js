9fd74b575627c93f4b945585dc3ac500f83b123e
Garpken <gknafey@gmail.com>
2019-11-02 01:42:51 -0400

    added Link tags to a few buttons

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 
 function AboutUsInfo() {
   return (
     
-        
         <div className="card text-white mb-3 aboutInfo">
         <div className="card-header">About Us</div>
             <div className="card-body">
@@ -13,10 +13,9 @@ function AboutUsInfo() {
             <p className="card-text">Our Company is committed to offering the best quality work possible at the right price.</p>
             </div>
 
-            <a><button type="button" className="btn btn-lg aboutButton">Learn More</button></a>
+            <Link to="/AboutUs"> <a><button type="button" className="btn btn-lg aboutButton">Learn More</button></a> </Link>
         </div>
 
-    
   );
 }
 
--- client/src/components/LinkBtn/index.js
+++ client/src/components/LinkBtn/index.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 
 function LinkBtn() {
   return (
     <span className="cover" tabIndex="0">
-      <a><button type="button" className="btn btn-lg btn-block">About Us</button></a>
-      <a><button type="button" className="btn btn-lg btn-block two">Request a Quote</button></a>
-      <a><button type="button" className="btn btn-lg btn-block three">Full Portfolio</button></a>
+      <Link to="/AboutUs"> <a><button type="button" className="btn btn-lg btn-block">About Us</button></a> </Link>
+      <Link to="">       <a><button type="button" className="btn btn-lg btn-block two">Request a Quote</button></a> </Link>
+      <Link to="/Portfolio"> <a><button type="button" className="btn btn-lg btn-block three">Full Portfolio</button></a> </Link>
     </span>
   );
 }

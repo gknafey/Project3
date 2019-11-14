6f8d6b160056788b7c8eec80752858cf11afe8f8
Garpken <gknafey@gmail.com>
2019-11-13 22:56:27 -0500

    added css code to the nav component to make the nav bar responsive to cell-phones

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,33 +9,33 @@ function Nav() {
 
       <Link to="/" className="navbar-brand"> <img className="logo" alt="Logo Here" src={Logo}></img> </Link>
 
-      <ul className="nav justify-content-end">
+      <ul className="nav justify-content-end navOrient1">
         <li className="nav-item">
           <Link to="/" className="nav-item test">Home </Link>
         </li>
         <li className="nav-item">
-          <p className="nav-item" style={{ "color": "#e5cdaa" }}>&nbsp;|&nbsp;</p>
+          <p className="nav-item center0" style={{ "color": "#e5cdaa" }}>&nbsp;|&nbsp;</p>
         </li>
         <li className="nav-item">
           <Link to="/AboutUs" className="nav-item test">About Us </Link>
         </li>
         <li className="nav-item">
-          <p className="nav-item" style={{ "color": "#e5cdaa" }} >&nbsp;|&nbsp;</p>
+          <p className="nav-item center0" style={{ "color": "#e5cdaa" }} >&nbsp;|&nbsp;</p>
         </li>
         <li className="nav-item">
           <Link to="/Portfolio" className="nav-item test">Portfolio </Link>
         </li>
         <li className="nav-item">
-          <p className="nav-item" style={{ "color": "#e5cdaa" }}>&nbsp;|&nbsp;</p>
+          <p className="nav-item disappear1" style={{ "color": "#e5cdaa" }}>&nbsp;|&nbsp;</p>
         </li>
         <li className="nav-item">
-          <Link to="/Services" className="nav-item test">Services </Link>
+          <Link to="/Services" className="nav-item test center1">Services </Link>
         </li>
         <li className="nav-item">
-          <p className="nav-item" style={{ "color": "#e5cdaa" }}>&nbsp;|&nbsp;</p>
+          <p className="nav-item center0" style={{ "color": "#e5cdaa" }}>&nbsp;|&nbsp;</p>
         </li>
         <li className="nav-item">
-          <Link to="/ContactUs" className="nav-item test">Contact Us </Link>
+          <Link to="/ContactUs" className="nav-item test center2">Contact Us </Link>
         </li>
       </ul>
     </nav>

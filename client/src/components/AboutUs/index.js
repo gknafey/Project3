08494279534212a5289c7ed674de7b9f1bc3daa7
Garpken <gknafey@gmail.com>
2019-12-10 15:08:50 -0500

    eidted the Quick about us section on the home page to display the content desired by the client

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -7,10 +7,12 @@ function AboutUsInfo() {
   return (
     
         <div className="card text-white mb-3 aboutInfo">
-        <div className="card-header card-header-resize">About Us</div>
+        <div className="card-header card-header-resize">Who We Are</div>
             <div className="card-body">
             <h5 className="card-title">Plaster Restorations LLC</h5>
-            <p className="card-text">Our Company is committed to offering the best quality work possible at the right price.</p>
+            <p className="card-text">Our company is licensed and insured to provide superior quality
+            plaster installation and repair solutions for both residential and 
+            commercial construction, throughout NJ and the surrounding areas.</p>
             </div>
 
             <Link to="/AboutUs"> <button type="button" className="btn btn-lg customBtn aboutButton">Learn More</button> </Link>

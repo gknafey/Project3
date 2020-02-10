b48692fdf6cb1f18a1df8405659fe8bd624669df
Garpken <gknafey@gmail.com>
2020-02-10 14:11:23 -0500

    Added another comparison in the portfolio and worked on the css alittle

--- client/src/components/Portfolio1/index.js
+++ client/src/components/Portfolio1/index.js
@@ -6,33 +6,47 @@ import Before1 from "../../images/4BEFORE.jpg"
 import After1 from "../../images/4AFTER.jpg"
 import Before2 from "../../images/7BEFORE.jpg"
 import After2 from "../../images/7AFTER.jpg"
+import Before3 from "../../images/mason_A_edited.JPG"
+import After3 from "../../images/mason_B_edited.JPG"
 
 
 
 function Portfolio1() {
-    
+
     return (
 
-        
-            <div className="media portfolio-size">
-            <div className="comparison-1">
-            <p className="name-style">3 Coat Plaster</p>
-            <div className="image-style-portfolio">
-            <ReactCompareImage leftImage={Before1} rightImage={After1} />
-            </div>
-            <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
-            </div>
 
-            <div className="comparison-2">
-            <p className="name-style">Ornamental Plaster</p>
-            <div className="image-style-portfolio">
-            <ReactCompareImage leftImage={Before2} rightImage={After2} />
-            </div>
-            <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
-            </div>
+        <div className="media portfolio-size">
+            
+                <div className="comparison-1">
+                    <p className="name-style">3 Coat Plaster</p>
+                    <div className="image-style-portfolio">
+                        <ReactCompareImage leftImage={Before1} rightImage={After1} />
+                    </div>
+                    <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
+                </div>
+
+                <div className="comparison-2">
+                    <p className="name-style">Ornamental Plaster</p>
+                    <div className="image-style-portfolio">
+                        <ReactCompareImage leftImage={Before2} rightImage={After2} />
+                    </div>
+                    <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
+                </div>
+          
+            {/* <span className="row2">
+                <div className="comparison-3">
+                    <p className="name-style">Masonic Lobby</p>
+                    <div className="image-style-portfolio">
+                        <ReactCompareImage leftImage={Before3} rightImage={After3} />
+                    </div>
+                    <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
+                </div>
+            </span> */}
+
         </div>
 
-        
+
 
 
 

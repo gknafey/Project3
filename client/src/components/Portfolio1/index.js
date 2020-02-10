07d4c2ee26a77a52d3c6d0fa79fc41931cc30d21
Garpken <gknafey@gmail.com>
2020-02-10 18:49:06 -0500

    I added some photos to be the portfolio and edited the css to improve the UI

--- client/src/components/Portfolio1/index.js
+++ client/src/components/Portfolio1/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import ReactCompareImage from 'react-compare-image';
-import Before1 from "../../images/4BEFORE.jpg"
-import After1 from "../../images/4AFTER.jpg"
-import Before2 from "../../images/7BEFORE.jpg"
-import After2 from "../../images/7AFTER.jpg"
-import Before3 from "../../images/mason_A_edited.JPG"
-import After3 from "../../images/mason_B_edited.JPG"
+import Before1 from "../../images/4BEFORE.jpg";
+import After1 from "../../images/4AFTER.jpg";
+import Before2 from "../../images/7BEFORE.jpg";
+import After2 from "../../images/7AFTER.jpg";
+import Before3 from "../../images/mason_A_edited.JPG";
+import After3 from "../../images/mason_B_edited.JPG";
+import Before4 from "../../images/mason_1_edited.JPG";
+import After4 from "../../images/mason_2_edited.JPG";
+import Before5 from "../../images/Buccleuch_1.JPG";
+import After5 from "../../images/Buccleuch_2.JPG";
 
 
 
@@ -15,34 +19,57 @@ function Portfolio1() {
 
     return (
 
-
         <div className="media portfolio-size">
-            
-                <div className="comparison-1">
+            <div className="row1">
+                <div className="col-md comparison-1">
                     <p className="name-style">3 Coat Plaster</p>
+                    <h4>( Pull Slider in the middle of the picture to view before and after images. )</h4>
                     <div className="image-style-portfolio">
                         <ReactCompareImage leftImage={Before1} rightImage={After1} />
                     </div>
                     <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
+                    
                 </div>
 
                 <div className="comparison-2">
+                    <p className="name-style">Masonic Lobby</p>
+                    <h4>( Pull Slider in the middle of the picture to view before and after images. )</h4>
+                    <div className="image-style-portfolio">
+                        <ReactCompareImage leftImage={Before4} rightImage={After4} />
+                    </div>
+                    <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
+                </div>
+                
+                <div className="comparison-2">
+                    <p className="name-style">Buccleuch</p>
+                    <h4>( Pull Slider in the middle of the picture to view before and after images. )</h4>
+                    <div className="image-style-portfolio">
+                        <ReactCompareImage leftImage={Before5} rightImage={After5} />
+                    </div>
+                    <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
+                </div>
+            </div>
+
+            <div className="row2">
+
+                <div className="comparison-3">
                     <p className="name-style">Ornamental Plaster</p>
+                    <h4>( Pull Slider in the middle of the picture to view before and after images. )</h4>
                     <div className="image-style-portfolio">
                         <ReactCompareImage leftImage={Before2} rightImage={After2} />
                     </div>
                     <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
                 </div>
-          
-            {/* <span className="row2">
+
                 <div className="comparison-3">
                     <p className="name-style">Masonic Lobby</p>
+                    <h4>( Pull Slider in the middle of the picture to view before and after images. )</h4>
                     <div className="image-style-portfolio">
                         <ReactCompareImage leftImage={Before3} rightImage={After3} />
                     </div>
                     <Link to="/RequestForm"><button type="button" className="btn btn-lg customBtn comparison-button">More Samples</button></Link>
                 </div>
-            </span> */}
+            </div>
 
         </div>
 

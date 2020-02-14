750ff1f14215ea2480769a5f2f595706234af483
Garpken <gknafey@gmail.com>
2020-02-14 16:10:13 -0500

    added an npm package for a dropdown button in the job management page, edited the refer button component for the drop down

--- client/src/components/ReferBtn/index.js
+++ client/src/components/ReferBtn/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import "./style.css";
 
-function ReferBtn(props) {
+const ReferBtn = (props) => {
+  const [dropdownOpen, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!dropdownOpen);
+
   return (
-    <span className="refer-btn" {...props} role="button" tabIndex="0">
-      Refer Job
-    </span>
+    // <span className="refer-btn" {...props} role="button" tabIndex="0">
+    //   Refer Job
+    // </span>
+
+    <ButtonDropdown className="refer-btn" {...props} role="button" tabIndex="0" isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle caret>
+        Refer Job
+      </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem header>Referees</DropdownItem>
+        <DropdownItem>Tim Artist</DropdownItem>
+        <DropdownItem>Jerry</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>David Smith</DropdownItem>
+      </DropdownMenu>
+    </ButtonDropdown>
+
   );
 }
 

8ae6bf1ab7d9d7f8be2b9e6225fcc9e662f4443e
Garpken <gknafey@gmail.com>
2020-02-22 19:58:33 -0500

    edited the css of the quick contact to make it more responsive

--- client/src/components/QuickContact/index.js
+++ client/src/components/QuickContact/index.js
@@ -5,7 +5,7 @@ function QuickContact() {
     return (
         <div className="quickContact">
             <div>
-                <p>&nbsp; Phone:<span className="phoneOrient"> <a className="phoneWork" href="tel:908-581-2151">908-581-2151</a> &nbsp;<span>|</span>&nbsp; Email:</span> <a className="email" href="mailto:plaster.restorations@gmail.com">plaster.restorations@gmail.com </a>&nbsp;</p>
+                <p className="phoneStart" >&nbsp; Phone:<span className="phoneOrient"> <a className="phoneWork" href="tel:908-581-2151">908-581-2151</a> &nbsp;<span>|</span>&nbsp; Email:</span> <a className="email" href="mailto:plaster.restorations@gmail.com">plaster.restorations@gmail.com </a>&nbsp;</p>
             </div>
         </div>
     )

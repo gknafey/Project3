c09048d9cf78b08a7df30db5ca293bcc3c92cfd7
Garpken <gknafey@gmail.com>
2020-02-24 18:00:57 -0500

    edited the css of the aboutus and requestAQuote components to make it more responsive

--- client/src/components/AboutImage/index.js
+++ client/src/components/AboutImage/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./style.css";
-import Image from "../../images/8.jpg"
+import Image from "../../images/8.jpg";
 
 function AboutImage() {
     return (
 
         
-
+            
             <img className="img fluid aboutImg" src={Image} alt="Porfolio Not Found"></img>
+            
            
         
 

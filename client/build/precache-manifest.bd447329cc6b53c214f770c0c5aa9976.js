bd336b92ef105a883299988ec5c6c4a2ecdbe7db
Garpken <gknafey@gmail.com>
2020-05-11 13:24:56 -0400

    ran npm run build

---
+++ client/build/precache-manifest.bd447329cc6b53c214f770c0c5aa9976.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ae8826b714772b823d323230c88009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d7b30df83d0aa49b368",
+    "url": "/static/css/2.deac9bf1.chunk.css"
+  },
+  {
+    "revision": "d929d25bfd6a326314f4",
+    "url": "/static/css/main.d5a5df9f.chunk.css"
+  },
+  {
+    "revision": "8d7b30df83d0aa49b368",
+    "url": "/static/js/2.5b8762a1.chunk.js"
+  },
+  {
+    "revision": "41cf88b16fcf9174072defbc265ae5cf",
+    "url": "/static/js/2.5b8762a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d929d25bfd6a326314f4",
+    "url": "/static/js/main.60062bd6.chunk.js"
+  },
+  {
+    "revision": "226fd4a5de4b477d8cce",
+    "url": "/static/js/runtime-main.6ff082a3.js"
+  },
+  {
+    "revision": "f7a7d00386c049396dd9b9485c0ec8ea",
+    "url": "/static/media/11test.f7a7d003.jpg"
+  },
+  {
+    "revision": "1527c045c12f14b3d051d775a004c1e1",
+    "url": "/static/media/4AFTER.1527c045.jpg"
+  },
+  {
+    "revision": "8f1e55599f5977808918c651a9224f23",
+    "url": "/static/media/4BEFORE.8f1e5559.jpg"
+  },
+  {
+    "revision": "57674417dce78a097db105bc1543eebd",
+    "url": "/static/media/7AFTER.57674417.jpg"
+  },
+  {
+    "revision": "790ba69398fcad7963d623f472c0b823",
+    "url": "/static/media/7BEFORE.790ba693.jpg"
+  },
+  {
+    "revision": "346d7bc6e697dfb538a9c92bb83aad77",
+    "url": "/static/media/8.346d7bc6.jpg"
+  },
+  {
+    "revision": "8530cc97ce53327a810373c79014f5bf",
+    "url": "/static/media/Buccleuch_1_edited.8530cc97.JPG"
+  },
+  {
+    "revision": "288d1acbeff6ac7d596e193d22fb6f61",
+    "url": "/static/media/Buccleuch_2_edited.288d1acb.JPG"
+  },
+  {
+    "revision": "b7f8073720435404a539a36bcf6282d7",
+    "url": "/static/media/COG_on_scaffold.b7f80737.jpg"
+  },
+  {
+    "revision": "42208a3385db0a114f0ce9f53f2d9af6",
+    "url": "/static/media/Church_On_The_Green_1.42208a33.JPG"
+  },
+  {
+    "revision": "a15bfc0e742795b7665ac716db776262",
+    "url": "/static/media/Church_On_The_Green_2.a15bfc0e.JPG"
+  },
+  {
+    "revision": "a3924ecb252ff4aa9ac032c85fb62bd9",
+    "url": "/static/media/Clients.a3924ecb.JPG"
+  },
+  {
+    "revision": "bd43ea9de0235c79a934379d0c13626e",
+    "url": "/static/media/NC_Grainger_Auditorium.bd43ea9d.JPG"
+  },
+  {
+    "revision": "46026828f6db65dd13eacd83f7396a3c",
+    "url": "/static/media/NC_top_right_1_edited.46026828.JPG"
+  },
+  {
+    "revision": "68dc04fb16e7a7d020e8be0655996e09",
+    "url": "/static/media/NC_top_right_2_edited.68dc04fb.JPG"
+  },
+  {
+    "revision": "249bbec7eea8846570bdd71816a27eaf",
+    "url": "/static/media/Trenton_Historic_Masonic_Temple.249bbec7.JPG"
+  },
+  {
+    "revision": "fd391323a660b38be674a9c0c71b5ac0",
+    "url": "/static/media/final.fd391323.png"
+  },
+  {
+    "revision": "2964935cb020a476e33d0d880f2c6a85",
+    "url": "/static/media/images1.2964935c.JPG"
+  },
+  {
+    "revision": "300deef86bb87a9c2373c3ddba8deab9",
+    "url": "/static/media/images2.300deef8.JPG"
+  },
+  {
+    "revision": "0ec7aab073fc5bc0cb388236ecd6756d",
+    "url": "/static/media/images3.0ec7aab0.JPG"
+  },
+  {
+    "revision": "aaa3b9c9ab95d543d08dacebddfaed11",
+    "url": "/static/media/mason_1_edited.aaa3b9c9.JPG"
+  },
+  {
+    "revision": "74e76a296a5c2747d3f2caca28f46f0b",
+    "url": "/static/media/mason_2_edited.74e76a29.JPG"
+  },
+  {
+    "revision": "8624c90d53b6764c082c7521293165cf",
+    "url": "/static/media/mason_A_edited.8624c90d.JPG"
+  },
+  {
+    "revision": "5b51014d7661778b93140f758632ab26",
+    "url": "/static/media/mason_B_edited.5b51014d.JPG"
+  }
+]);
